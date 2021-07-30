adf6da72ef84cdcdfb81c5a2036febbebe16953f
prashast-systango <psitlani@isystango.com>
2021-07-30 20:55:32 +0530

    Products added, products page updated

--- Shopping24x7/app/views.py
+++ Shopping24x7/app/views.py
@@ -22,8 +22,27 @@ class HomeView(View):
 # def home(request):
 #     return render(request, 'app/home.html')
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', {'product':product})
+
+
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+
+class MobileView(View):
+    def get(self, request, data=None):
+        if data == None:
+            mobiles = Product.objects.filter(category='M')
+        elif data == 'Oneplus' or data == 'Samsung' or data == 'Apple' or data == 'Redmi':
+            mobiles = Product.objects.filter(category='M').filter(brand=data)
+
+        
+        return render(request, 'app/mobile.html', {'mobiles':mobiles})
+        
+
+
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -43,8 +62,6 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
 
 def login(request):
  return render(request, 'app/login.html')
